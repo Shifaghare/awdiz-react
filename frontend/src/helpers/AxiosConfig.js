d7ef1d51cd0a416c1c68d43fba8134db9c027fb8
Shifaghare <138295553+Shifaghare@users.noreply.github.com>
2023-12-10 20:41:37 +0530

    updates

--- frontend/src/helpers/AxiosConfig.js
+++ frontend/src/helpers/AxiosConfig.js
@@ -18,7 +18,8 @@ import axios from "axios";
 var BackendUrl = 'http://localhost:8000/api/v1'
 
 if(process.env.MODE === "PRODUCTION"){
-
+    BackendUrl="https://backend-ezut.onrender.com/api/v1"
+ 
 }
 const api = axios.create({baseURL: BackendUrl})
 
